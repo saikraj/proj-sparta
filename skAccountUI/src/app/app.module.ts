cf60c05d437aed735c82432bf20b5547cebc86ce
Sairaj Kumar <sakumar@zuora.com>
2021-09-15 21:49:44 +0530

    login

--- skAccountUI/src/app/app.module.ts
+++ skAccountUI/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './account/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
